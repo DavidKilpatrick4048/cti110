174250e4c5cc972cf9fde6dd8d58bf1da09979f0
DavidKilpatrick4048 <kilpatrd4048@student.faytechcc.edu>
2024-09-11 23:29:27 -0400

    Add files via upload
    P1LAB1 First Submission

---
+++ P1LAB1_KilpatrickDavid.py
@@ -0,0 +1,9 @@
+#David Kilpatrick
+#9/11/2024
+#P1LAB1
+#Program to welcome users to the course
+print('Enter your first name:', end=' ')
+first_name = input()
+print('Enter your last name:', end=' ')
+last_name = input()
+print('Hello', first_name, last_name,'! Welcome to CTI-110')
